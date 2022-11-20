6c7820b4f9e196786ec8e9315d12125a6e283583
minhazpro3 <minhazpro1@gmail.com>
2022-11-20 22:26:47 +0600

    project setup process file

---
+++ project.setup.process.js
@@ -0,0 +1,16 @@
+/* project create & setup  {
+    1. create a folder
+    2. npm init -y
+    3. npm install : cors, express, mondodb, mongoose, dotenv, nodemon 
+    4. create app.js file 
+    5. create root file kind of index.js
+    6. create .env file in root of project
+    7. create .gitignore file in root of project
+    8. import packages {optional}
+    9. use middleware json and cors {optional}
+    10. create a get function and give response
+    11. create PORT url
+    12. create listen function 
+    13. package.json file setup
+    14. finally npm run start-dev
+} */
